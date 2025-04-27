69f09b6994dc863620186b90cd1726e644c67053
Matt Liu <liuyychn@outlook.com>
2025-04-27 10:43:24 +0800

    开发模式下热更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -33,13 +34,25 @@ module.exports = {
     ],
   },
   devServer: {
+    // 静态文件夹目录
     static: './public',
+    // 浏览端口
     port: 3000,
+    // 在浏览器中打开
     open: true,
+    // 启用热部署
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
+    // 这个插件会在开发模式下启用 React Fast Refresh
+    // 使得 React 组件在热更新时能够保持状态
+    // 注意：在生产模式下不需要这个插件，生产模式下会自动禁用
+    new ReactRefreshWebpackPlugin({
+      // 不在界面上显示报错
+      overlay: false
+    })
   ],
 };
